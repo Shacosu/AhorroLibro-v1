8ca71ebd692dfe0fd2c8f78de309966f762e35fd
Shacosu <sh4c0p@gmail.com>
2025-04-01 17:23:50 -0300

    (Fix): Monitor books

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -6,12 +6,14 @@ const router = express.Router();
 
 // Rutas protegidas que requieren autenticación
 router.route('/').get(authenticateToken, getAllBooks);
-router.route('/:id').get(authenticateToken, getBookById);
 
-// Otras rutas
+// Rutas específicas (deben ir antes de las rutas con parámetros dinámicos)
 router.route('/processLists').get(monitorAndProcessLists);
 router.route('/addList').post(addUserList);
 router.route('/monitorBooks').get(monitorBooks);
 router.route('/add-book').post(addBookToUser);
+
+// Rutas con parámetros dinámicos (deben ir después de las rutas específicas)
+router.route('/:id').get(authenticateToken, getBookById);
 
 export default router;
